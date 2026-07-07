8b37673e49d575aac30d334e99f4ac67a8466904
prasiddhax <125749747+prasiddhax@users.noreply.github.com>
2026-07-07 11:39:11 +0545

    link update3

--- projects.data.ts
+++ projects.data.ts
@@ -22,7 +22,7 @@ export const WORKS_DATA: Project[] = [
         type: "image",
         width: "full",
         image: {
-src: "https://drive.google.com/thumbnail?id=1kXsJWL8my0I8VGEJf9VbOEFkKofGg3m0&sz=w2000"
+src: "https://drive.google.com/thumbnail?id=1kXsJWL8my0I8VGEJf9VbOEFkKofGg3m0&sz=w2000" ,
     aspect: "16/9",
           caption: "AITC International — full brand system",
         },
